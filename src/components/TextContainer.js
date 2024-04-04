adf7300a486cf33978cb428c26f7358e92618f35
KiiKang <63621971+KiiKang@users.noreply.github.com>
2024-04-03 21:51:15 -0400

    update text

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -10,23 +10,57 @@ const TextContainer = (props) => {
     // const [content, setContent] = useState(null);
     // const [loading, setLoading] = useState(true);
     const ref = useRef();
+    const bibRef = useRef();
+
     useEffect(() => {
+        bibRef.current.style.visibility = "hidden"
         ref.current.scroll({
             top: 0
         })
+        const spans = ref.current.querySelectorAll('span');
+        spans.forEach(span => {
+            span.addEventListener('mouseenter', (e) => {
+                handleBib(e);
+            });
+            span.addEventListener('mouseout', () => {
+                hideBib();
+            });
+        });
+        // let ps = Array.prototype.slice.call(ref.current.children).filter(d => d.localName === "p")
+        // ps.forEach(p => {
+        //     let spans = Array.prototype.slice.call(p.children).filter(d => d.localName === "span")
+        //     console.log(spans);
+        // })
     }, [props.id])
 
+    const handleBib = (e) => {
+        let bib = textData.bib.filter(d => d.match === e.target.innerText)[0]
+        bibRef.current.innerHTML = ""
+        if (bib.author) bibRef.current.innerHTML += bib.author + ". "
+        bibRef.current.innerHTML += "<i>" + bib.title + "</i>. "
+        if (bib.publisher) bibRef.current.innerHTML += bib.publisher + ", "
+        bibRef.current.innerHTML += bib.year + "."
+        if (bib.page) bibRef.current.innerHTML += " p." + bib.page
+        // bibRef.current.style.width = "fit-content"
+        bibRef.current.style.visibility = "visible"
+        if (e.target.offsetLeft < 250) bibRef.current.style.left = e.target.offsetLeft + 'px'
+        else bibRef.current.style.right = 520 - e.target.offsetLeft - e.target.offsetWidth + 'px'
+        bibRef.current.style.top = e.target.offsetTop + 25 + 'px'
+    }
+    const hideBib = () => {
+        bibRef.current.style.visibility = "hidden"
+    }
     const bibify = (str) => {
         let str_bibified = str
         textData.bib.forEach(b => {
-            let lastName = b.author.substring(b.author.indexOf(" ") + 1)
-            str_bibified = str_bibified.replaceAll(lastName, "<span class='hover:underline decoration-red-600 underline-offset-1'>" + lastName + "</span>")
+            str_bibified = str_bibified.replaceAll(b.match,
+                "<span key={lastName} className='hover:underline decoration-red-600 underline-offset-1'>" + b.match + "</span>")
         })
         return str_bibified
     }
 
     return (
-        <div className='Intro-textbox w-[520px] p-7 text-lg fixed hyphens-auto min-h-fit max-h-[80%] overflow-y-scroll'
+        <div className='Intro-textbox overflow-x-clip w-[520px] p-7 text-lg fixed hyphens-auto min-h-fit max-h-[80%] overflow-y-scroll'
         ref={ref}>
             <br/>
             <center>
@@ -45,6 +79,9 @@ const TextContainer = (props) => {
                 </Link>
             </center>
             <br/>
+            <div ref={bibRef}
+                 className='fixed leading-tight pointer-events-none max-w-[80%] min-w-[200px] w-fit left-0 top-0 bg-white border-2 border-gray-700 p-1 font-sans text-sm'
+            />
         </div>
     )
 }
